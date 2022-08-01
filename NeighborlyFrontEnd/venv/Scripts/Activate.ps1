3046673436265f94f21098e9688374d390acc1da
tung nguyen <tung.nguyenthanh@petronas.com>
2022-08-01 15:57:26 +0700

    update webapp

--- NeighborlyFrontEnd/venv/Scripts/Activate.ps1
+++ NeighborlyFrontEnd/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="E:\WEB\Flask\Azure_function\venv"
+$env:VIRTUAL_ENV="E:\Udacity Projects\Project 2\nd081-c2-Building-and-deploying-cloud-native-applications-from-scratch-project-starter\NeighborlyFrontEnd\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
